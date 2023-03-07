8438cd0d741b8ff61a4378f0e0bbaf517d296679
azamatmavlonov <119999901+azamatmavlonov@users.noreply.github.com>
2023-03-07 20:56:55 +0500

    Добавьте файлы проекта.

---
+++ Pharma.API/Pharma.API.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Pharma.API.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
